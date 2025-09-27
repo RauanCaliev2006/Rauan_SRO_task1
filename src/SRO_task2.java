e36925a499a1c8166cb7b9849d4333e709d122fe
Rauan <rayancaliev@gmail.com>
2025-09-27 15:29:18 +0500

    СРО 1

---
+++ src/SRO_task2.java
@@ -0,0 +1,12 @@
+public class SRO_task2 {
+        public static void main(String[] args) {
+            int a = 4, b = 7;
+            int c = 11, d = -2, e = -3;
+            int f = 41, g = -5, h = -6, i = 1;
+
+            System.out.println("Сумма 2 чисел: " + (a + b));
+            System.out.println("Сумма 3 чисел: " + (c + d + e));
+            System.out.println("Сумма 4 чисел: " + (f + g + h + i));
+        }
+
+}
